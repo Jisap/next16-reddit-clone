c75a9882c1cc85d86316bff29d336418718b7516
Jorge Matos <jisapjisap@gmail.com>
2026-05-27 16:12:03 +0200

    setup

---
+++ app/(core)/layout.tsx
@@ -0,0 +1,20 @@
+import LeftSidebar from '@/components/layout/left-sidebar'
+import Navbar from '@/components/layout/Navbar'
+
+import React from 'react'
+
+const CoreGroupLayout = async ({ children }: { children: React.ReactNode }) => {
+  return (
+    <>
+      <Navbar />
+      <div className="mx-auto flex max-w-[1200px] gap-8 px-4 pb-16 pt-2">
+        <LeftSidebar />
+        <div className='min-w-0 flex-1'>
+          {children}
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default CoreGroupLayout
---
+++ providers/neon-auth-ui-provider.tsx
@@ -0,0 +1,19 @@
+"use client"
+
+import { createAuthClient } from "@neondatabase/auth/next"
+import { NeonAuthUIProvider } from "@neondatabase/auth/react"
+
+
+const authClient = createAuthClient();
+
+export default function NeonAuthProviders({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <NeonAuthUIProvider authClient={authClient} defaultTheme="dark">
+      {children}
+    </NeonAuthUIProvider>
+  );
+}
